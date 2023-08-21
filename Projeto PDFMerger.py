fdd9dd9aa0747cd49e596263884c0443f2d7a5c4
Thiago Zupelli <thiago.zupelli@hotmail.com>
2023-08-21 20:28:35 -0300

    Updated project code.

--- Projeto PDFMerger.py
+++ Projeto PDFMerger.py
@@ -3,11 +3,11 @@ import os
 
 merger = PyPDF2.PdfMerger()
 
-lista_arquivos = os.listdir("arquivos")
-lista_arquivos.sort()
+lista_de_arquivos = os.listdir("arquivos") # Localiza o diretório que contém os arquivos que serão unificados em um novo PDF
+lista_de_arquivos.sort() # Ordena os arquivos PDF
 
-for arquivo in lista_arquivos:
+for arquivo in lista_de_arquivos: # Loop que irá ler os arquivos PDF e unificar em um arquivo final
     if ".pdf" in arquivo:
         merger.append(f"arquivos/{arquivo}")
 
-merger.write("PDF final.pdf")
+merger.write("PDF final.pdf") # Finalização do código para criação do novo arquivo com os PDFs unificados
